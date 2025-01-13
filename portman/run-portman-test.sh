6a26f55a12e0ec83b2cab8c0a811d5e2b8fdfd6c
NoodleSugar <noel.rodriguez@hotmail.fr>
2025-01-13 17:32:22 +0100

    Fix portman config

--- portman/run-portman-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-script_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$script_dir"
-
-portman \
-	--cliOptionsFile    portman-cli.yml \
-	--portmanConfigFile portman-config.yml
